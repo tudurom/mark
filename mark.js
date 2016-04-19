f91a62908fa319bfa96004a0d47ea94b98c4b8df
Tudor <xenogenesis@openmailbox.org>
2016-04-19 19:24:21 +0300

    add: error handling

--- mark.js
+++ mark.js
@@ -1,10 +1,25 @@
 #!/usr/bin/node
+
+function usage() {
+    console.error("usage: mark <file>");
+}
+
 var marked = require('marked');
 var fs = require('fs');
 
+if (!process.argv[2]) {
+    usage();
+    process.exit(1);
+}
+
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
     if (err) {
-        return console.log(err);
+        if (err.code == 'ENOENT') {
+            console.error(`${process.argv[2]} - no such file`);
+        } else {
+            console.error(err);
+        }
+        process.exit(1);
     }
 
     marked.setOptions({
